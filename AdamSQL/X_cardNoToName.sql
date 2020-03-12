4d96ef41a89b255c70164a6546feee7f4d186833
Kerch13 <62117199+Kerch13@users.noreply.github.com>
2020-03-12 18:01:59 -0400

    2 more procedures

---
+++ AdamSQL/X_cardNoToName.sql
@@ -0,0 +1,6 @@
+CREATE PROCEDURE `X_cardNoToName` (IN cardNoInput int)
+BEGIN
+	SELECT b.name
+	FROM borrower b
+    WHERE cardNo=cardNoInput;
+END
---
+++ AdamSQL/X_existingCopies.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE `X_existingCopies` (IN branchIdInput int, IN bookIdInput int)
+BEGIN
+	SELECT bc.noOfCopies
+	FROM tbl_book_copies bc, tbl_library_branch lb
+    WHERE lb.branchId=branchIdInput
+		AND bc.bookId=bookIdInput;
+END
