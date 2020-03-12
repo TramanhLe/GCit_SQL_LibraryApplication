5581f34bafdb44bfd3ce77ee82186ade5e2740c3
Will Cao <kingdragael@hotmail.com>
2020-03-12 18:03:29 -0400

    included fetchProcedure and borrower question edited

--- borrower_questions.py
+++ borrower_questions.py
@@ -1,27 +1,34 @@
+import fetchProcedures
+
 def question_one(self):
     self.id = input("Enter your card number.\n")
-    # Valid card number check here boolean
-    card = True
+    # Valid card number check here boolean -check-
+    result=fetchProcedures.validateCardNo(self.id)
+    card = None
+    if len(result)==0:
+        print("Not a valid ID")
+    else:
+        card=True
+    
     if card == True:
         self.choice = input("1) Check out a book\n 2) Return a book\n")
         self.next()
     else:
         pass
 
-# 
 
 def question_two(self):
     if self.choice == "1":
-        # Get library branches here
-        branches = ["New York", "New Mexico", "New Hampshire", "New Zealand"]
-        for i, count in enumerate(branches):
-            print(count)
-        branch = input(""" Which branch do you want 
-                          to check out from?\n 1) {branches[0]}\n
-                           2) {branches[1]}\n
-                            3) {branches[2]}""")
+        # Get library branches here -check-
+        branches=fetchProcedures.fetchBranchs()
+        for x in branches:
+            print(x)
+        branchInput = input(" Which branch do you want to check out from?")
+        #using which branchInput to grab list of books from the proper branch
         self.next()
     elif self.choice == "2": 
-        # Get books this borrower has checked out
-        books = ["Romeo","Juliet","Ron Howard"]
+        # Get books this borrower has checked out -check-
+        books=fetchProcedures.fetchBorrowerBooks(self.id)
+        for x in branches:
+            print(x)
         self.next()
--- fetchProcedures.py
+++ fetchProcedures.py
@@ -1,3 +1,4 @@
+#Connection to DB
 import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
@@ -9,13 +10,31 @@ myCursor = mydb.cursor()
 
 results=[]
 
+
+#Done
 def fetchBranchs():
     myCursor.callproc('sp_showAllBranch')
     for x in myCursor.stored_results():
         results=x.fetchall()
     return results
 
+def validateCardNo(input):
+    myCursor.callproc('sp_validateCardNo',[input])
+    for x in myCursor.stored_results():
+        results=x.fetchone()
+    return results
+
+def fetchBorrowerBooks(input):
+    myCursor.callproc('sp_fetchBorrowerBooks',[input])
+    for x in myCursor.stored_results():
+        results=x.fetchall()
+    return results
+
+
+
+# Not done
 def fetchBranchInfo():
+    #fetch branch info
     myCursor.callproc('sp_showAllBranch')
     for x in myCursor.stored_results():
         results=x.fetchall()
