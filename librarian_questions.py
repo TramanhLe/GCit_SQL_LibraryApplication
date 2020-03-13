1ee35fdedde45c4c69a9a01ea996e2fa9fa66fe4
William Marchek <39922188+wilbobraggins@users.noreply.github.com>
2020-03-13 14:07:01 -0400

    second question

--- librarian_questions.py
+++ librarian_questions.py
@@ -2,7 +2,7 @@ import fetchProcedures
 import start_questions
 import questions
 
-def lib_question(self):
+def lib_question_one(self):
     if self.choice == "1":
         res = (fetchProcedures.fetchBranchs())
         out = ""
@@ -15,6 +15,10 @@ def lib_question(self):
 def lib_question_two(self):
     int_choice = int(self.choice)
     branches = (fetchProcedures.fetchBranchs())
-    branches[int_choice]
-    fetchProcedures.fetchBranchIdByName(branches[int_choice])
-    
+    str_branch = (''.join(branches[int_choice]))
+    self.id = fetchProcedures.fetchBranchIdByName(str_branch)
+    self.choice = input("Press 1) Update the details of the Library\nPress 2) Add copies of Book to the Branch\n : ")
+    if self.choice == "1":
+        print(f'You have chosen to update the Branch with Branch Id: {self.id} and Branch Name: {str_branch}.')
+        new_branch_name = input(f"Please Enter New Branch Name for {str_branch}\n:")
+        
