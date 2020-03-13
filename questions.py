7e0d6d60e497de523813f0e048c94c7194512889
William Marchek <39922188+wilbobraggins@users.noreply.github.com>
2020-03-13 12:34:29 -0400

    added loop for lib questions and start question

--- questions.py
+++ questions.py
@@ -1,11 +1,13 @@
 import borrower_q_check_out
 import borrower_q_return
+import librarian_questions
 import start_questions
 
 # Each int : array within the values of borrower, librarian, and admin
 question_bank = {
     "start": {
         "borrower": start_questions.borrower_question_one,
+        "librarian": start_questions.lib_question_one,
     },
     "borrower": {
         # check_out
@@ -15,7 +17,7 @@ question_bank = {
     },
     "librarian": {
         1: [],
-        2: []
+        2: [],
     },
     "admin": {
         1: [],
@@ -34,5 +36,8 @@ for name, val in borrower_q_return.__dict__.items():
     if callable(val):
         question_bank["borrower"][2].append(val)
 
+for name, val in librarian_questions.__dict__.items():
+    if callable(val):
+        question_bank["librarian"][1].append(val)
 
 
