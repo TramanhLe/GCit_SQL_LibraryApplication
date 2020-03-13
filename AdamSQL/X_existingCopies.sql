485b0c6b2f718085ad4eeb5149189090bab3d462
Kerch13 <62117199+Kerch13@users.noreply.github.com>
2020-03-12 20:50:14 -0400

    fixed X_existingCopies
    needed a GROUP BY branchName

--- AdamSQL/X_existingCopies.sql
+++ AdamSQL/X_existingCopies.sql
@@ -3,5 +3,6 @@ BEGIN
 	SELECT bc.noOfCopies
 	FROM tbl_book_copies bc, tbl_library_branch lb
     WHERE lb.branchId=branchIdInput
-		AND bc.bookId=bookIdInput;
+		AND bc.bookId=bookIdInput
+	GROUP BY lb.branchName;
 END
