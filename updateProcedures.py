ba74fa0673f79a23d8bb9265574d312b61ffbe01
William Marchek <39922188+wilbobraggins@users.noreply.github.com>
2020-03-13 14:55:01 -0400

    added updateBranchName

--- updateProcedures.py
+++ updateProcedures.py
@@ -20,6 +20,9 @@ def updatedBookCopies(branchId,bookId,numCopies):
     print("Book copies has been updated")
 
 
+def updateBranchName(curBranch, newBranch):
+    myCursor.callproc('sp_updateBranchName', [curBranch, newBranch])
+    print("Branch Name has been changed")
 
 
 
