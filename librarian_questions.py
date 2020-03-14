07c1eeddd1eda9aad872676ad57b05675b9668c6
William Marchek <39922188+wilbobraggins@users.noreply.github.com>
2020-03-14 16:35:45 -0400

    update branch name and address

--- librarian_questions.py
+++ librarian_questions.py
@@ -1,7 +1,3 @@
-import fetchProcedures
-import start_questions
-import questions
-
 def lib_question_one(self):
     if self.choice == "1":
         res = (fetchProcedures.fetchBranchs())
@@ -9,7 +5,7 @@ def lib_question_one(self):
         for i, val in enumerate(res):
             output = f"{i}) {val}\n"
             out += output
-        self.choice = input("Please Select Branch number or q to quit to previous:\n" + out)
+        self.choice = input("Select Branch Number or q to quit to previous:\n" + out)
         self.next()
 
 def lib_question_two(self):
@@ -20,5 +16,21 @@ def lib_question_two(self):
     self.choice = input("Press 1) Update the details of the Library\nPress 2) Add copies of Book to the Branch\n : ")
     if self.choice == "1":
         print(f'You have chosen to update the Branch with Branch Id: {self.id} and Branch Name: {str_branch}.')
-        new_branch_name = input(f"Please Enter New Branch Name for {str_branch}\n:")
-        
+        new_branch_name = input(f"Please Enter New Branch Name for {str_branch}\n or enter N/A for no change:")
+        if len(new_branch_name) > 0:
+            updateProcedures.updateBranchName(f"{self.id[0]}",f"{str_branch}",new_branch_name)
+        else:
+             print('No Changes for Branch Name')
+        pass
+        new_address = input(f'Please enter new branch address for {new_branch_name} or enter N/A for no change:\n')
+        if len(new_address) > 0:
+            updateProcedures.updateBranchAddress(f"{self.id[0]}",new_address)
+            self.prev()
+        else:
+            print('No Changes for Branch Address')
+    if self.choice == "2":
+        self.next()
+
+
+def lib_update_address(self):
+    print('yay')
